44e027255cea8f181b485e0f83135c06694ced29
Yankhoba <bagayoko.yan@gmail.com>
2025-05-28 16:30:09 +0200

    Refactor templates and URLs for improved structure and clarity

--- DjangoEvaluation/sakila/urls.py
+++ DjangoEvaluation/sakila/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import status, countries, city
+from .views import status, countries, city, city_detail
 
 urlpatterns = [
     path('', status, name = ('banniere') ),
     path('countries/', countries, name='countries'),
     path('cities/', city, name='city'),
-    path('cities/<int:city_id>/', city, name='city_detail'),
-
+    path('cities/<int:city_id>/detail/', city_detail, name='city_detail'),
 ]
